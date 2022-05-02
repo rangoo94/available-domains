d5fb311612a452b6c3d0a807f52bd0c16f7059dc
Dawid Rusnak <dawid@drcode.pl>
2022-05-02 19:10:51 +0200

    expose DomainProcessor

--- index.ts
+++ index.ts
@@ -1,2 +1,3 @@
 export { getAvailableDomains } from './src/getAvailableDomains';
 export { isDomainAvailable } from './src/isDomainAvailable';
+export { DomainProcessor } from './src/DomainProcessor';
